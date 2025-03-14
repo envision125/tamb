07e64a8895c2d70751490c5497c9c6131595a225
envision125 <126027195+envision125@users.noreply.github.com>
2025-03-13 21:19:12 -0700

    image clean up- remove unwanted images

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Tortas Ahogadas Mi barrio';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const dashLink = 'https://www.doordash.com/store/tortas-ahogadas-mi-barrio-oakland-28048743/';

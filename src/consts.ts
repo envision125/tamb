40bf47facda500f295088ee3827eb0b98a7cbb66
envision125 <126027195+envision125@users.noreply.github.com>
2025-06-04 13:43:43 -0700

    sync new style

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tortas Ahogadas Mi barrio';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Tortas Ahogadas Mi barrio, Jalisco!';
 export const dashLink = 'https://www.doordash.com/store/tortas-ahogadas-mi-barrio-oakland-28048743/';
 
 

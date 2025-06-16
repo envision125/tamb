f9dbf090d7f4b67f5a0b41cd706f4adec31dc3bd
envision125 <126027195+envision125@users.noreply.github.com>
2025-06-16 15:54:45 -0700

    added a menu section

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,9 @@
 
 export const SITE_TITLE = 'Tortas Ahogadas Mi barrio';
 export const SITE_DESCRIPTION = 'Tortas Ahogadas Mi barrio, Jalisco!';
+export const SUBTITLE= "¡ESTILO GUADALAJARA!"
 export const dashLink = 'https://www.doordash.com/store/tortas-ahogadas-mi-barrio-oakland-28048743/';
+export const yelp = 'https://www.yelp.com/biz/tortas-ahogadas-mi-barrio-oakland?adjust_creative=mapquest&hrid=yKrPS3IP3btKH_1u8AteCg&utm_campaign=yelp_feed&utm_medium=feed_v2&utm_source=mapquest';
 
 
 

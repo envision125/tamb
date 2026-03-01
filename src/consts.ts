88f995f15d521fc0c46e667e8e9650a57c523c2f
envision125 <126027195+envision125@users.noreply.github.com>
2026-03-01 15:20:37 -0800

    fix locatio and index page, headers size and mobile size

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tortas Ahogadas Mi barrio';
-export const SITE_DESCRIPTION = 'Tortas Ahogadas Mi barrio, Jalisco!';
+export const SITE_TITLE = 'Tortas Ahogadas Mi Barrio';
+export const SITE_DESCRIPTION = 'Tortas Ahogadas Mi Barrio, Jalisco!';
 export const SUBTITLE= "¡Estilo Guadalajara!"
 export const dashLink = 'https://www.doordash.com/store/tortas-ahogadas-mi-barrio-oakland-28048743/';
 export const yelp = 'https://bit.ly/46bTH50';
